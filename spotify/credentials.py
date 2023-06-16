7772f6df29c8ed04f6fdf410396049d924432726
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2023-06-16 21:18:33 +0530

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -4,8 +4,8 @@
 
 # CLIENT_ID = os.getenv("CLIENT_ID")
 # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-CLIENT_ID = "03af24ce12024fb2aaa9c02f33670dd3"
-CLIENT_SECRET = "52e6d11e7a884ef8b61ae322fdf66ade"
+CLIENT_ID = "ID"
+CLIENT_SECRET = "SECRET"
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
 
 print(CLIENT_ID, CLIENT_SECRET)
